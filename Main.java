7008ba6c1723c939b1ba3702e01d94ab6e1bd6c6
Jociel Alves de Jesus <Jociel61211327>
2025-11-13 17:21:55 -0300

    alteracao na branch

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ public Class Main{
 public static void main(String[] args) {
 
     System.out.println("Hello World!");
+    System.out.println("TEste de Merge sem conflito");
+     var nome  = "SAMUEL";
+
+
+
 }
 }
